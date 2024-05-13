def3882482568fbe19f36cd2fa5886d777f6f0d3
Kakhaber <kakha81@gmail.com>
2024-05-13 16:53:43 +0400

    css correction in Header.tsx

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -4,9 +4,15 @@ import style from "./Header.module.css";
 export default function Header() {
   return (
     <header className={style.container}>
-      <Link href="/">Main Page</Link>
-      <Link href="/aboutus">Kakha`s Page</Link>
-      <Link href="/mentors">List of Mentors</Link>
+      <Link className={style.link} href="/">
+        Main Page
+      </Link>
+      <Link className={style.link} href="/aboutus">
+        Kakha`s Page
+      </Link>
+      <Link className={style.link} href="/mentors">
+        List of Mentors
+      </Link>
     </header>
   );
 }
